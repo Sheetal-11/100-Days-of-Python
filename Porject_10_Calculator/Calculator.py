f89edcb363ad1306fbc2649ea4e068bcff9b0944
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-20 11:28:13 +0530

    Add Calculator project

---
+++ Porject_10_Calculator/Calculator.py
@@ -0,0 +1,22 @@
+# Calculator
+
+import calculator_art
+
+#Addition
+def add(n1, n2):
+    return n1 + n2
+
+#Subtraction
+def subtract(n1, n2):
+    return n1 - n2
+
+#Multiplication
+def multiply(n1, n2):
+    return n1 * n2
+
+#Division
+def division(n1, n2):
+    return n1 / n2
+
+
+
