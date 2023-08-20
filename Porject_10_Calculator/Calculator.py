35d7999670972ff79c3f4bab72ad3ce07808e3f7
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-20 12:54:10 +0530

    Add calculator function
    This allows the user if they want to continue an existing result or
    start fresh with a new calculation

--- Porject_10_Calculator/Calculator.py
+++ Porject_10_Calculator/Calculator.py
@@ -26,32 +26,35 @@ operations = {
         "/" : division,
         }
 
-num1 = int( input("What's the first number?: "))
 
-
-end_calculation = False
-
-while not end_calculation:
+def calculator():
     
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Pick an operation: ")
-    num_2 = int( input("What's the next number?: "))
-        
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num_2)
-
-    print(f"{num1} {operation_symbol} {num_2} = {answer}")
-        
-    should_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to to exit: ").lower()
+    num1 = int( input("What's the first number?: "))
     
-    if should_continue == 'y':
-        num1 = answer
+    end_calculation = False
+    
+    while not end_calculation:
         
-    if should_continue == 'n':
-        end_calculation = True
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num_2 = int( input("What's the next number?: "))
+            
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num_2)
+    
+        print(f"{num1} {operation_symbol} {num_2} = {answer}")
+            
+        should_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ").lower()
+        
+        if should_continue == 'y':
+            num1 = answer
+            
+        if should_continue == 'n':
+            end_calculation = True
+            calculator()
 
-
+calculator()
 
 
 
