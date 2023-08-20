24db589f052a8a3c89061c208db482fb18a7c0bb
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-20 11:49:12 +0530

    Add operations dictionary and caluculation_function variable
    calculation_function variable calls the desired operation function

--- Porject_10_Calculator/Calculator.py
+++ Porject_10_Calculator/Calculator.py
@@ -19,4 +19,23 @@ def division(n1, n2):
     return n1 / n2
 
 
+operations = {
+        "+" : add,
+        "-" : subtract,
+        "*" : multiply,
+        "/" : division,
+        }
+
+num1 = int( input("What's the first number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int( input("What's the second number?: "))
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+
 
