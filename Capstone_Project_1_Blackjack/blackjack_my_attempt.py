30e5d52f5b486ee9d9b473f22a3e9bdc7151901f
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-22 13:49:07 +0530

    add logo

--- Capstone_Project_1_Blackjack/blackjack_my_attempt.py
+++ Capstone_Project_1_Blackjack/blackjack_my_attempt.py
@@ -12,12 +12,13 @@
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
-import random
+from random import choice
+from art_blackjack import logo
 
 def pick_card():
     
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    return random.choice(cards)
+    return choice(cards)
 
 
 def score(hand):
@@ -101,10 +102,9 @@ def declare_winner(player_total, dealer_total, dealer_cards):
                 print("You lose!")
             
     
-
 def game_start():
     
-    print("\n\n")
+    print(logo)
     print("Welcome to the Blackjack game!")
         
     #Intialize player's and dealer's cards
