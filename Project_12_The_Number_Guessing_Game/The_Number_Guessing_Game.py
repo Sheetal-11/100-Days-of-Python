c4849a9e2474a71b1156aa6793545176505fca2d
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-22 11:50:52 +0530

    Set difficulty level and number of lives

--- Project_12_The_Number_Guessing_Game/The_Number_Guessing_Game.py
+++ Project_12_The_Number_Guessing_Game/The_Number_Guessing_Game.py
@@ -10,5 +10,19 @@ print("I'm thinking of a number between 1 and 100.")
 num = randint(1, 100)
 print(f"Pssst, the correct answer is {num}")
 
+#Set number of lives
+print("\nChoose your difficulty level")
+lives = 0
 
+while lives == 0:
+    difficulty_level = input("'easy' or 'hard': ")
+    
+    if difficulty_level == "easy":
+        lives = 10
+    elif difficulty_level == "hard":
+        lives = 5
+    else:
+        print("Please type a valid answer")
+
+print(f"lives = {lives}")
 
