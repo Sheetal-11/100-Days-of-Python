d90ac08fe4f26e0b0b7d675caa5c6beb19dbc9a8
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-24 18:06:01 +0530

    Add pick_entry function
    It returns a random entry from data

--- Project_14-Higher_Lower_Game/Higher_Lower_Game.py
+++ Project_14-Higher_Lower_Game/Higher_Lower_Game.py
@@ -1,4 +1,20 @@
 # Higher Lower Game
 
+from game_data import data
+from art import logo, vs
+from random import randint
+
+# 4 keys: 'name', 'follower_count', 'description', 'country'
+
+def pick_entry():
+    '''
+    returns a random entry from data
+    '''
+    position = randint(0, len(data) -1)
+    return data[position]
+    
+
+print(pick_entry())
+
 
 
