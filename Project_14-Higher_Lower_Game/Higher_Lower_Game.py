ef8c77b60c003121136ee2d68cb3a77677f973d8
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-24 18:14:26 +0530

    Assign random entries to variables A and B

--- Project_14-Higher_Lower_Game/Higher_Lower_Game.py
+++ Project_14-Higher_Lower_Game/Higher_Lower_Game.py
@@ -13,8 +13,12 @@ def pick_entry():
     position = randint(0, len(data) -1)
     return data[position]
     
+    
+A = pick_entry()
+B = pick_entry()
 
-print(pick_entry())
+print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
+print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
 
 
 
