92c2ba3376de715409e4600b0d1aaecead52a040
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-24 18:25:21 +0530

    Let the user select an entry

--- Project_14-Higher_Lower_Game/Higher_Lower_Game.py
+++ Project_14-Higher_Lower_Game/Higher_Lower_Game.py
@@ -12,13 +12,32 @@ def pick_entry():
     '''
     position = randint(0, len(data) -1)
     return data[position]
-    
+
+def user_choice(var_a, var_b):
+    '''
+    Takes 2 dictionary entries and returns the one that the user selects
+
+    '''
+    print("\nWho has more followers?")
+    user_input = input("'A' or 'B': ").lower()
+    if user_input == 'a':
+        choice = var_a
+    elif user_input == 'b':
+        choice = var_b
+    else:
+        print("Please type a valid option")
+        choice = user_choice(var_a, var_b)
+    return choice
+
     
 A = pick_entry()
 B = pick_entry()
 
 print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
 print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
+
+choice = user_choice(A, B)
+print(choice)
 
 
 
