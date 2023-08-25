5b3a98c493911bdef4cf3e4d873b3077d726d156
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-25 16:58:34 +0530

    print serve statement

--- Project_15_Coffee_Machine/main.py
+++ Project_15_Coffee_Machine/main.py
@@ -130,7 +130,7 @@ while not turn_off:
             if tr_pass:
                 resources = update_stock(resources, prompt)
                 money += MENU[prompt]["cost"]
-                report()
+                print(f"Here is your {prompt}. Enjoy!")
 
         else:
             print("If you'd like please select something else")
@@ -141,20 +141,3 @@ while not turn_off:
         turn_off = True
     else:
         print("Please enter a valid input.")
-
-
-# todo 7. Make Coffee.
-# a. If the transaction is successful and there are enough resources to make the drink the user selected,
-# then the ingredients to make the drink should be deducted from the coffee machine resources.
-# E.g. report before purchasing latte:
-# Water: 300ml
-# Milk: 200ml
-# Coffee: 100g
-# Money: $0
-# Report after purchasing latte:
-# Water: 100ml
-# Milk: 50ml
-# Coffee: 76g
-# Money: $2.5
-# b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”.
-# If latte was their choice of drink.
