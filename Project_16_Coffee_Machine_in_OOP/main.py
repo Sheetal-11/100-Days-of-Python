0ed029c5a8cb0217fb8274005f727bd89dc6e2d5
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-26 14:37:16 +0530

    Prompt to ask user what they'd like

--- Project_16_Coffee_Machine_in_OOP/main.py
+++ Project_16_Coffee_Machine_in_OOP/main.py
@@ -1,3 +1,8 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Ask the user what they'd like
+menu = Menu()
+prompt = input(f"What would you like? {menu.get_items()}: ")
+print(prompt)
