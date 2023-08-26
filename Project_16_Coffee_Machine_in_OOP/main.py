ad7cdd942a1eb58a13b3d702c8b70b5fb891af42
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-26 14:42:51 +0530

    Add turn_off functionality to the machine

--- Project_16_Coffee_Machine_in_OOP/main.py
+++ Project_16_Coffee_Machine_in_OOP/main.py
@@ -2,7 +2,14 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# Ask the user what they'd like
-menu = Menu()
-prompt = input(f"What would you like? {menu.get_items()}: ")
-print(prompt)
+turn_off = False
+
+while not turn_off:
+
+    # Ask the user what they'd like
+    menu = Menu()
+    prompt = input(f"What would you like? {menu.get_items()}: ")
+    print(prompt)
+
+    if prompt == "off":
+        turn_off = True
