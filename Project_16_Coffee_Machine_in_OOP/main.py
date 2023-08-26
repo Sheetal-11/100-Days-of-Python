930f651da0961280bd60c763904a4dae1641b05d
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-26 15:31:10 +0530

    Add more if-else statements to cover all possible inputs

--- Project_16_Coffee_Machine_in_OOP/main.py
+++ Project_16_Coffee_Machine_in_OOP/main.py
@@ -2,20 +2,29 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-coffee = CoffeeMaker()
-money = MoneyMachine()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
 turn_off = False
 
 while not turn_off:
 
     # Ask the user what they'd like
-    menu = Menu()
-    prompt = input(f"What would you like? {menu.get_items()}: ")
-    print(prompt)
+    prompt = input(f"What would you like? ({menu.get_items()}): ")
 
     if prompt == "report":
-        coffee.report()
-        money.report()
+        coffee_maker.report()
+        money_machine.report()
 
     elif prompt == "off":
         turn_off = True
+
+    elif prompt in ["espresso", "latte", "cappuccino"]:
+        coffee = menu.find_drink(prompt)
+        print("coffee = ", coffee)
+        print("coffee.name = ", coffee.name)
+        print("coffee.cost = ", coffee.cost)
+        print("coffee.ingredients = ", coffee.ingredients)
+
+    else:
+        print("Please enter a valid option.")
