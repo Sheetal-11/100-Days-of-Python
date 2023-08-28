3b5e365b2ad48a0d4497e4727831e9de231f6199
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-28 17:05:42 +0530

    Add next_question method to QuizBrain

--- Project_17_Quiz_Game/main.py
+++ Project_17_Quiz_Game/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -8,3 +9,6 @@ for entry in question_data:
     answer = entry["answer"]
     new_question = Question(question, answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
