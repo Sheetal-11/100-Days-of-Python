81c4a20def368208247037f85c0939d4fdbbcc55
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-30 11:10:14 +0530

    Make a pop-up to get user's input

--- Project_19_Turtle_Race/Turtle_Race.py
+++ Project_19_Turtle_Race/Turtle_Race.py
@@ -3,5 +3,8 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)   # set screen size
+# Bring up a pop-up
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+print(user_bet)
 
 screen.exitonclick()
