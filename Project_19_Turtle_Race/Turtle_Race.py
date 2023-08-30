fc0a47367d9e762d6b1cc2a9f008ff83f19db7ad
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-30 12:42:05 +0530

    Create new turtles

--- Project_19_Turtle_Race/Turtle_Race.py
+++ Project_19_Turtle_Race/Turtle_Race.py
@@ -1,13 +1,21 @@
 from turtle import Turtle, Screen
 
-tim = Turtle(shape="turtle")
 screen = Screen()
 screen.setup(width=500, height=400)   # set screen size
 # Bring up a pop-up
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 
-# Make the turtle go to the start of the line (to the left edge)
-tim.up()   # to stop turtle from drawing when going from origin to new location
-tim.goto(x=-230, y =-100)
+all_turtles = []   # list where we will store all turtle objects
+colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
+gap = 0
+
+for new_color in colors:
+    # for each color create a new turtle
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(new_color)
+    all_turtles.append(new_turtle)
+    new_turtle.up()  # to stop the turtle from drawing when going from origin to new location
+    new_turtle.goto(x=-230, y=-125+gap)
+    gap += 40   # vertical gap between each turtle
 
 screen.exitonclick()
