49d2f8e024529aed51435fb9d4170b907e7f10d6
Sheetal <sheetal.mishra.117@gmail.com>
2023-08-30 11:26:18 +0530

    Get the turtle to starting position

--- Project_19_Turtle_Race/Turtle_Race.py
+++ Project_19_Turtle_Race/Turtle_Race.py
@@ -1,10 +1,13 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+tim = Turtle(shape="turtle")
 screen = Screen()
 screen.setup(width=500, height=400)   # set screen size
 # Bring up a pop-up
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
+
+# Make the turtle go to the start of the line (to the left edge)
+tim.up()   # to stop turtle from drawing when going from origin to new location
+tim.goto(x=-230, y =-100)
 
 screen.exitonclick()
